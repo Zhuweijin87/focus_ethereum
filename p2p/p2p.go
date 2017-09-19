69f4a17ed6e472bc683edb2bf5ddabbee3737abc
Weijin Zhu <472214158@qq.com>
2017-09-19 18:50:06 +0800

    Update p2p.go

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -2,30 +2,44 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/p2p"
 )
 
-const messageID = 0
-
-type Message string
-
-func MyProtocal() p2p.Protocol {
-	return p2p.Protocol {
-		Name : "MyProtocal",
-		Version: 1,
-		Length: 1,
-		Run: msgHandle,
+func randomID() (id p2p.discover.NodeID) {
+	for i := range id {
+		id[i] = byte(rand.Intn(255))
 	}
+	return id
 }
 
-func msgHandler(p2p *p2p.Peer, ws p2p.MsgReaderWriter) error {
-	for {
-		msg, err := ws.ReadMsg()
-		if err != nil {
-			return err
-		}
-		
+func newServer(id discover.NodeID, pf func(*Peer)) *p2p.Server {
+	uniqueKey, _ := crypto.GenerateKey()
+	config := p2p.Config{
+		Name:       "test",
+		MaxPeers:   10,
+		ListenAddr: "192.168.1.195:8200",
+		PrivateKey: uniqueKey,
 	}
+
+	serv := p2p.Server{
+		Config:       config,
+		newPeerHook:  pf,
+		newTransport: func(fd net.Conn) transport { return newTestTransport(id, fd) },
+	}
+
+	if err := serv.Start(); err != nil {
+		log.Println("fail to start server: ", err)
+	}
+
+	return serv
+}
+
+func servListen() {
+	conn := make(chan *p2p.Peer)
+	randId := randomID()
+
+	srv := newServer()
+	
 }
