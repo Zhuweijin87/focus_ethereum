570e7e4d8fa31794aaca1de6cd6637cf938e58bd
mydev <mydev@mydevdeiMac.local>
2017-09-23 14:59:34 +0800

    update

---
+++ p2p/02_peers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"errors"
+	"github.com/ethereum/go-ethereum/p2p"
+)
+
+var errProtocolReturned = errors.New("protocol returned")
+var baseProtocolLength  = uint64(16)
+
+func main() {
+	proto := p2p.Protocol {
+		Name: "Test",
+		Length: 5,
+		Run : func(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
+			if err := p2p.ExpectMsg(rw, 2, []uint{1}); err != nil {
+				fmt.Println("ExpectMsg 2:", err)
+				return err
+			}
+			if err := p2p.ExpectMsg(rw, 3, []uint{2}); err != nil {
+				fmt.Println("ExpectMsg 3:", err)
+				return err
+			}
+			if err := p2p.ExpectMsg(rw, 4, []uint{3}); err != nil {
+				fmt.Println("ExpectMsg 3:", err)
+				return err
+			}
+			return nil
+		},
+	}
+
+	closer, rw, _, errc := p2p.MyTestPeer([]p2p.Protocol{proto})
+	defer closer()
+
+	p2p.Send(rw, baseProtocolLength+2, []uint{1})
+	p2p.Send(rw, baseProtocolLength+3, []uint{2})
+	p2p.Send(rw, baseProtocolLength+4, []uint{3})
+
+	select {
+	case err := <-errc:
+		if err != errProtocolReturned {
+			fmt.Printf("peer returned error: %v\n", err)
+			return 
+		}
+	case <-time.After(2 * time.Second):
+		fmt.Printf("receive timeout")
+		return 
+	}
+}
