509f7b66dbd166267d513dc4cff555a883375966
zuvakin <weijin@nerthuschain.org>
2017-10-12 19:01:25 +0800

    update

--- p2p/peer.go
+++ p2p/peer.go
@@ -8,31 +8,32 @@ import (
 )
 
 var errProtocolReturned = errors.New("protocol returned")
-var baseProtocolLength  = uint64(16)
 
 func main() {
-	proto := p2p.Protocol {
-		Name: "Test",
+	baseProtocolLength := uint64(16)
+	proto := p2p.Protocol{
+		Name:   "a",
 		Length: 5,
-		Run : func(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
-			fmt.Println("Run Protocol")
+		Run: func(peer *p2p.Peer, rw p2p.MsgReadWriter) error {
 			if err := p2p.ExpectMsg(rw, 2, []uint{1}); err != nil {
-				fmt.Println("ExpectMsg 2:", err)
-				return nil
+				fmt.Println(err)
+				return err 
 			}
+			fmt.Println("Expect Msg 2")
 			if err := p2p.ExpectMsg(rw, 3, []uint{2}); err != nil {
-				fmt.Println("ExpectMsg 3:", err)
-				return nil
+				fmt.Println(err)
+				return err 
 			}
+			fmt.Println("Expect Msg 3")
 			if err := p2p.ExpectMsg(rw, 4, []uint{3}); err != nil {
-				fmt.Println("ExpectMsg 3:", err)
-				return nil
+				fmt.Println(err)
+				return err 
 			}
+			fmt.Println("Expect Msg 4")
 			return nil
 		},
 	}
 
-	// MyTestPeer 在mytest.go 文件中，用于测试
 	closer, rw, _, errc := p2p.MyTestPeer([]p2p.Protocol{proto})
 	defer closer()
 
@@ -44,10 +45,8 @@ func main() {
 	case err := <-errc:
 		if err != errProtocolReturned {
 			fmt.Printf("peer returned error: %v\n", err)
-			return 
 		}
 	case <-time.After(2 * time.Second):
-		fmt.Printf("receive timeout")
-		return 
+		fmt.Printf("receive timeout\n")
 	}
 }
