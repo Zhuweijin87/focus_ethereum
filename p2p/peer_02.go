6fe38b800f0a4d828d2d586216f27cb419478013
Weijin Zhu <472214158@qq.com>
2017-10-15 23:12:07 +0800

    update

--- p2p/peer_02.go
+++ p2p/peer_02.go
@@ -15,14 +15,30 @@ func randomID() (id discover.NodeID) {
 	return id
 }
 
-func main() {
+// 简单Peer创建
+func SimplePeer() {
 	var id discover.NodeID 
 	caps := []p2p.Cap{{"foo", 2}, {"bar", 3}}
 	id = randomID()
-
 	peer := p2p.NewPeer(id, "MyPeer", caps)
-
-	fmt.Println(peer.ID(), peer.Name())
-
 	fmt.Println(peer.String())
+}
+
+func NormalPeer() {
+	protocol := p2p.Protocol{
+		Name: "Normal Peer",
+		Length: 5,
+		Run: func(peer *p2p.Peer, rw *p2p.MsgReadWriter) error {
+
+			return nil
+		},
+	}
+
+	caps := []p2p.Cap{{"foo", 2}, {"bar", 3}}
+	peer := p2p.NewPeer(randomID(), "My Peer", caps)
+}
+
+func main() {
+	SimplePeer()
+
 }
